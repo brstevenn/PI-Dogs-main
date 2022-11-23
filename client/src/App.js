fa5080bb45353f086a1a344c3d25b494cd0a22e5
brstevenn <brayangaitan81@gmail.com>
2022-11-23 00:52:21 -0500

    App

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import './App.css'
+
+
+import Home from './components/Home/Home';
+import Dogs from './components/Dogs/Dogs';
+import DogDetails from './components/Dogs/DogCard/DogDetails/DogDetails';
+import Form from './components/Form/Form'
+//import InsertEpisode from './components/InsertEpisode/InsertEpisode';
+//import InsertCharacter from './components/InsertCharacter/InsertCharacter';
+//import NotFound from './components/NotFound/NotFound';
+//import Loader from './components/Loader/Loader';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>Henry Dogs</h1>
+    <div className="app">
+      <div className="routes" >
+        <Switch>
+          <Route path="/dogs" ><Dogs/></Route>
+          <Route path="/form" ><Form/></Route>
+          <Route path="/dogdetails" ><DogDetails/></Route>
+          <Route path="/" ><Home/></Route>
+        </Switch>
+      </div>
     </div>
   );
+  
 }
 
 export default App;
