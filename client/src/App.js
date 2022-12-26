2ee27f3b0cf0ec755106bd80b9eb15c093bf0d97
brstevenn <brayangaitan81@gmail.com>
2022-12-26 13:04:43 -0500

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,37 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import axios from "axios";
 
-import './App.css'
+import "./App.css";
 
+import Home from "./components/Home/Home";
+import Dogs from "./components/Dogs/Dogs";
+import DogDetails from "./components/Dogs/DogCard/DogDetails/DogDetails";
+import Form from "./components/Form/Form";
 
-import Home from './components/Home/Home';
-import Dogs from './components/Dogs/Dogs';
-import DogDetails from './components/Dogs/DogCard/DogDetails/DogDetails';
-import Form from './components/Form/Form'
-//import InsertEpisode from './components/InsertEpisode/InsertEpisode';
-//import InsertCharacter from './components/InsertCharacter/InsertCharacter';
-//import NotFound from './components/NotFound/NotFound';
-//import Loader from './components/Loader/Loader';
-
+axios.defaults.baseURL = "http://localhost:3001/";
 
 function App() {
-
   return (
     <div className="app">
-      <div className="routes" >
+      <div className="routes">
         <Switch>
-          <Route path="/dogs" ><Dogs/></Route>
-          <Route path="/form" ><Form/></Route>
-          <Route path="/dogdetails" ><DogDetails/></Route>
-          <Route path="/" ><Home/></Route>
+          <Route path="/dogs">
+            <Dogs />
+          </Route>
+          <Route path="/form">
+            <Form />
+          </Route>
+          <Route path="/dogdetails">
+            <DogDetails />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
         </Switch>
       </div>
     </div>
   );
-  
 }
 
 export default App;
--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const GET_ALL_DOGS = "GET_ALL_DOGS";
 export const GET_DOGS_BY_NAME = "GET_DOGS_BY_NAME";
@@ -9,111 +9,122 @@ export const GET_DOGS_BY_TEMPERAMENTS = "GET_DOGS_BY_TEMPERAMENTS";
 export const SORT_DOGS = "SORT_DOGS";
 export const GET_DOG_DETAILS = "GET_DOG_DETAILS";
 
-const baseUrl = 'http://localhost:3001'
-
 export const getAllDogs = () => {
-	return async function  (dispatch){
-        return await axios.get(`${baseUrl}/dogs`)
-        .then(async res => {
-            await dispatch({
-                type: GET_ALL_DOGS,
-                payload: res.data
+    return async function (dispatch) {
+        return await axios
+            .get(`/dogs`)
+            .then(async (res) => {
+                await dispatch({
+                    type: GET_ALL_DOGS,
+                    payload: res.data,
+                });
             })
-        })
-        .catch(err => {console.log(err)})
-    }
-}
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+};
 
 export const getDogsByName = (name) => {
-    return async function(dispatch){
-        return await axios.get(`${baseUrl}/dogs?name=${name}`)
-        .then(res => {
-            dispatch({
-                type: GET_DOGS_BY_NAME,
-                payload: res.data
+    return async function (dispatch) {
+        return await axios
+            .get(`/dogs?name=${name}`)
+            .then((res) => {
+                dispatch({
+                    type: GET_DOGS_BY_NAME,
+                    payload: res.data,
+                });
             })
-        })
-        .catch(err => {console.log(err)})
-    }
-}
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+};
 
 export const setDog = (dog) => {
-    return async function(dispatch){
-        return await axios.post(`${baseUrl}/dogs`, dog)
-        .then(res => {
-            dispatch({
-                type: CREATE_DOG,
-                payload: dog
+    return async function (dispatch) {
+        return await axios
+            .post(`/dogs`, dog)
+            .then((res) => {
+                dispatch({
+                    type: CREATE_DOG,
+                    payload: dog,
+                });
             })
-        })
-        .catch(err => {console.log(err)})
-    }
-}
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+};
 
 export const deletDog = (name) => {
-    return function(dispatch){
+    return function (dispatch) {
         return dispatch({
             type: DELETE_DOG,
-            payload: name
-        })
-    }
-}
+            payload: name,
+        });
+    };
+};
 
 export const getAllTemperaments = () => {
-    return async function(dispatch){
-        return await axios.get(`${baseUrl}/temperaments`)
-        .then(res => {
-            dispatch({
-                type: GET_ALL_TEMPERAMENTS,
-                payload: res.data
+    return async function (dispatch) {
+        return await axios
+            .get(`/temperaments`)
+            .then((res) => {
+                dispatch({
+                    type: GET_ALL_TEMPERAMENTS,
+                    payload: res.data,
+                });
             })
-        })
-        .catch(err => {console.log(err)})
-    }
-}
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+};
 
 export const getDogsByTemperament = (name) => {
-    return function(dispatch){
+    return function (dispatch) {
         return dispatch({
             type: GET_DOGS_BY_TEMPERAMENTS,
-            payload: name
-        })
-    }
-}
+            payload: name,
+        });
+    };
+};
 
-export const sortDogs= (method) => {
-    return function(dispatch){
+export const sortDogs = (method) => {
+    return function (dispatch) {
         return dispatch({
             type: SORT_DOGS,
-            payload: method
-        })
-    }
-}
+            payload: method,
+        });
+    };
+};
 
 export const getDogDetails = (id, db) => {
-    console.log(db === "true")
-    if(db === "true"){
-        return async function(dispatch){
-            return await axios.get(`${baseUrl}/dogs/${id}?db=${true}`)
-            .then(res => {
-                console.log(res.data)
-                dispatch({
-                    type: GET_DOG_DETAILS,
-                     payload: [res.data]
+    if (db === "true") {
+        return async function (dispatch) {
+            return await axios
+                .get(`/dogs/${id}?db=${true}`)
+                .then((res) => {
+                    console.log(res.data);
+                    dispatch({
+                        type: GET_DOG_DETAILS,
+                        payload: [res.data],
+                    });
                 })
-            })
-            .catch(err => console.log(err))
-        }
-    } else if(db === "undefined" || db === "false") {
-        return async function(dispatch){
-            return await axios.get(`${baseUrl}/dogs/${id}?db=${false}`)
-            .then(res => {
-                dispatch({
-                    type: GET_DOG_DETAILS,
-                     payload: res.data
+                .catch((err) => console.log(err));
+        };
+    } else if (db === "undefined" || db === "false") {
+        return async function (dispatch) {
+            return await axios
+                .get(`/dogs/${id}?db=${false}`)
+                .then((res) => {
+                    dispatch({
+                        type: GET_DOG_DETAILS,
+                        payload: res.data,
+                    });
                 })
-            })
-            .catch(err => console.log(err))
-        }
+                .catch((err) => console.log(err));
+        };
     }
-}
+};
